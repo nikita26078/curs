88d6f2d5ba4763964da2d1e6d2f4fe6e654c55cb
nikita26078 <@>
2022-03-30 19:32:48 +0300

    Remove unused dependency

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['timetable-rtk.herokuapp.com/', '127.0.0.1']
 INSTALLED_APPS = [
     'jazzmin',
     'timetable',
-    'django_addanother',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
